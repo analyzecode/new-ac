0a11f9253af3e0fddc9aa6f3a3d41e6754f41450
analyzecode <analyzecode11@gmail.com>
2022-12-29 20:45:15 +0530

    ..

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-x = 5 ;                          // assigning 5 in x
-console.log( x ) ;              // printing the value of x
-const x ;                          // declaring variable x after being used.
+// x = 5 ;                          // assigning 5 in x
+// console.log( x ) ;              // printing the value of x
+// const x ;                          // declaring variable x after being used.
