dbd6f3b638844499d650a95edbd18fce2d06571c
analyzecode <analyzecode11@gmail.com>
2022-12-29 15:40:02 +0530

    ..

--- description.md
+++ description.md
@@ -1,3 +1,6 @@
+
+
+
 _tutorials , exercises ,  games --->_
 
 
---
+++ html-files/js-tutorials/js-tut-script.js
@@ -0,0 +1,88 @@
+
+   const landingpage = document.querySelector(".landingpage"),
+   footer = document.querySelector(".footer"),
+   sidebar = document.querySelector(".tut-content-sidebar"),
+   sharePool = document.querySelector(".share-pool-block"),
+   countbtn = document.querySelector(".countme");
+let btncount = document.querySelector(".count");
+let count = 0;
+fetch('topaarea.html')
+.then(component => component.text())
+.then(data=>{
+  landingpage.innerHTML =data
+});
+fetch('sharePool.html')
+.then(component => component.text())
+.then(data=>{
+  sharePool.innerHTML = data
+});
+fetch('footer.html')
+.then(component => component.text())
+.then(data=>{
+  footer.innerHTML = data
+});
+fetch('sidebar.html')
+.then(component => component.text())
+.then(data=>{
+  sidebar.innerHTML=data
+})
+function unsuffPass(){
+let password = document.validform.password.value; 
+if(password.length<6){
+  alert("Password must be at least 6 characters long.");  
+}
+}
+countbtn.addEventListener("click", () =>{
+      count++;
+      btncount.innerHTML = count;
+});
+let emojiblock = document.querySelector(".emojiblock");
+let emojiContent = ["&#128512;","&#128513;","&#128514;","&#128515;","&#128516;","&#128517;","&#128518;"]
+let emojiCount= 0;
+emojiblock.addEventListener("click", () => {
+  emojiCount++;
+  for(let i=0;i<emojiContent.length;i++){
+      if(i == emojiCount){
+          document.getElementById("emojichange").innerHTML = emojiContent[emojiCount];
+      }
+      if(emojiCount>emojiContent.length){
+      emojiCount = -1;
+  }
+  }   
+});
+let colorChangeBtn = document.querySelector(".colorshangeblock");
+let colorsList = ["#0556f3","#A555EC","#332FD0","#FF7000","#22A39F","#0014FF","#001253","#FF6464","#54B435"]
+let colorCount = 0;
+colorChangeBtn.addEventListener("click", () => {
+colorCount++;
+for(let i=0;i<colorsList.length;i++){
+  if(i == colorCount){
+      colorChangeBtn.style.backgroundColor = colorsList[i];
+  }
+  if(colorCount>colorsList.length){
+      colorCount = -1;
+  }
+}
+})
+let detectos = document.querySelector("button.detectos");
+detectos.addEventListener("click", () => {
+  document.getElementById("userBrowseinfo").innerHTML = navigator.appVersion;
+});
+let whatsappShareBtn = document.querySelector(".whatsappShareBtn");
+whatsappShareBtn.href = "whatsapp://send?text=" + document.URL;
+let tweetBtn = document.querySelector(".tweetBtn");
+tweetBtn.href= "https://twitter.com/intent/tweet?url="+document.URL;
+let faceBookBtn = document.querySelector(".faceBookBtn");
+faceBookBtn.href = "http://www.facebook.com/share.php?u="+document.URL;
+let linkdInBtn = document.querySelector(".linkdInBtn");
+linkdInBtn.href = "https://www.linkedin.com/sharing/share-offsite/?url="+document.URL;
+
+// js code to set auto active state to side btn [class = class="tutitemactive"]
+let jstutList = document.getElementsByClassName("sidebar-href");
+window.addEventListener("load",() => {
+for (let i = 0; i < jstutList.length; i++) {
+ if(jstutList[i].href == document.URL){
+      jstutList[i].classList.add("tutitemactive");
+ }
+}
+});
